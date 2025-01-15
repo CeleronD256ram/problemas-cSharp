ae739c1110cc7b7d4ac99dac48b3d7110fedcd3f
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-01-15 00:15:58 -0300

    Projeto refeito para explorar mais a lógica.

--- problemas-cSharp/GerenciadorDeTarefas/Program.cs
+++ problemas-cSharp/GerenciadorDeTarefas/Program.cs
@@ -1,7 +1,51 @@
 ﻿
-using System.Xml.Serialization;
-using problemas_cSharp.GerenciadorDeTarefas;
+namespace problemas_cSharp.GerenciadorDeTarefas;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tarefa tarefa = new Tarefa();
+            int opcao;
+            do
+            {
+                Console.WriteLine("===============================================");
+                Console.WriteLine("           MENU DE CONTROLE DE ROTINA        ");
+                Console.WriteLine("===============================================");
+                Console.WriteLine("1 - Adicionar uma nova tarefa");
+                Console.WriteLine("2 - Listar suas tarefas");
+                Console.WriteLine("3 - Remover uma tarefa da lista");
+                Console.WriteLine("0 - Sair");
+                
+                opcao = int.Parse(Console.ReadLine());
 
-
-Menu menu = new Menu();
-menu.ExibirMenu();
+                switch (opcao)
+                {
+                    case 1:
+                        tarefa.adiciona();
+                        break;
+                    
+                    case 2:
+                        tarefa.listarTarefas();
+                        break;
+                    
+                    case 3:
+                        tarefa.removerTarefa();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        break;
+                }
+                
+            } while (opcao != 0);
+            
+           
+            
+            
+            
+            
+            
+        }
+        
+    
+    }
--- problemas-cSharp/GerenciadorDeTarefas/Tarefa.cs
+++ problemas-cSharp/GerenciadorDeTarefas/Tarefa.cs
@@ -1,6 +1,80 @@
+using System.Collections;
+
 namespace problemas_cSharp.GerenciadorDeTarefas;
 
 public class Tarefa
 {
+
     
+    public ArrayList listaTarefas = new ArrayList();
+    
+ 
+    public void adiciona() 
+    {
+        Console.WriteLine("Digite o nome da tarefa:");
+        string nome = Console.ReadLine();
+
+        listaTarefas.Add(nome);
+        
+    }
+    
+ 
+    public void listarTarefas()
+    {
+        if (listaTarefas.Count > 0)
+        {
+            Console.WriteLine();
+            foreach (string tarefa in listaTarefas)
+            {
+                Console.Write(tarefa + " | ");
+            }   
+            
+        }
+        else
+        {
+            Console.WriteLine("Sua lista não possui nenhuma tarefa");
+        }
+        
+        
+        
+    }
+    
+    
+
+    public void removerTarefa()
+    {
+        Console.WriteLine("Digite o nome da tarefa que deseja remover: :");
+        string nome = Console.ReadLine();
+
+        if (listaTarefas.Contains(nome))
+        {
+            listaTarefas.Remove(nome);
+            Console.WriteLine("Tarefas removido com sucesso!");
+            
+        }
+        
+        Console.WriteLine("A tarefa digitada não foi encontrada!");
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
 }
