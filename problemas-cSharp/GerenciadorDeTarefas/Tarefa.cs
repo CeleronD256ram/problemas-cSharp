7bd35672139d8d54457d310088529887fd96257d
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-01-14 23:34:00 -0300

    Iniciando refatoração completa.

--- problemas-cSharp/GerenciadorDeTarefas/Tarefa.cs
+++ problemas-cSharp/GerenciadorDeTarefas/Tarefa.cs
@@ -1,43 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace problemas_cSharp.GerenciadorDeTarefas;
 
-namespace problemas_cSharp.GerenciadorDeTarefas
+public class Tarefa
 {
-
-
-    enum PRIORIDADE { Urgente, Importante, Moderada }
-    class Tarefa
-    {
-
-        public string Nome { get; private set; }
-        public PRIORIDADE NivelDePrioridade { get; private set; }
-
-
-        public Tarefa(string nome, PRIORIDADE prioridade)
-        {
-
-            if (string.IsNullOrWhiteSpace(nome))
-            {
-                throw new ArgumentException("O nome da tarefa não pode ser vazio");
-            }
-
-            Nome = nome;
-            NivelDePrioridade = prioridade;
-
-
-        }
-
-        public override string ToString()
-        {
-            return $"Tarefa: {Nome}  |  Prioridade: {NivelDePrioridade}";
-
-        }
-
-
-
-    }
+    
 }
